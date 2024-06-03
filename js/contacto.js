7215fce1c4a9b272708322685fdd0377a374afa7
Micaela Ansaldi <micansaldi@gmail.com>
2024-06-03 18:47:07 -0300

    formulario validado. responsivo interesyexcursiones

--- js/contacto.js
+++ js/contacto.js
@@ -21,36 +21,26 @@ document.addEventListener("DOMContentLoaded", function () {
          console.error('Error al obtener la lista de países:', error);
      });
 
- // Validacion del formulario
- document.getElementById('form').addEventListener('submit', function (event) {
-     var nombre = document.getElementById('nombre').value;
-     var apellido = document.getElementById('apellido').value;
-     var email = document.getElementById('email').value;
-     var pais = document.getElementById('pais').value;
-     var consulta = document.getElementById('consulta').value;
 
-     if (!nombre || !apellido || !email || !pais || !consulta) {
-         alert('Por favor, completa todos los campos obligatorios.');
-         event.preventDefault();
-     }
-     document.getElementById('mensajeEnviado').style.display = 'block';
-    
- });
-
-
-
-// submit
-const $form = document.querySelector('#form');
-
-$form.addEventListener('submit', btnSubmit);
-
-function btnSubmit(event) {
+// Validacion y envio del formulario
+document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault();
-    const form = new FormData(this);
-    console.log(form.get('nombre'));
-    const mailtoLink = `mailto:micansaldi@gmail.com?subject=${form.get('nombre')} ${form.get('apellido')}, ${form.get('pais')}, ${form.get('email')}&body=${form.get('consulta')}`;
+    
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var email = document.getElementById('email').value;
+    var pais = document.getElementById('pais').value;
+    var consulta = document.getElementById('consulta').value;
+
+    if (!nombre || !apellido || !email || !pais || !consulta) {
+        alert('Por favor, completa todos los campos obligatorios.');
+        return;
+    }
+    
+    const mailtoLink = `mailto:micansaldi@gmail.com?subject=${nombre} ${apellido}, ${pais}, ${email}&body=${consulta}`;
     window.location.href = mailtoLink;
-}
+
+    document.getElementById('mensajeEnviado').style.display = 'block';
 });
 
 //archivo
@@ -58,3 +48,4 @@ document.getElementById('archivo').addEventListener('change', function() {
     var fileName = this.files[0] ? this.files[0].name : 'No se ha seleccionado archivo';
     document.getElementById('file-name').textContent = fileName;
 });
+})
